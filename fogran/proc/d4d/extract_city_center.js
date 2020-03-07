0e9e6d2377cb0e86d1e6469a5ba141277d744bab
Zhihan JIANG <cc77888@qq.com>
2020-03-07 23:46:23 +0800

    Quick commit

--- fogran/proc/d4d/extract_city_center.js
+++
@@ -1,6 +0,0 @@
-var polygonCenter = require("geojson-polygon-center");
-
-var city_border = require("./data/dakar/city_border.json");
-
-var centroid = polygonCenter(city_border.geometry);
-console.log(centroid);
